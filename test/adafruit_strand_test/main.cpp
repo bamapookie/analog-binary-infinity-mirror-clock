7d5974ffb100dbe28132fa61c586d39dfed70f76
Shawn Kovalchick <bamapookie@gmail.com>
2020-01-24 17:03:18 -0500

    Updating tests to use all 6 strands now that I have the last two wired up

--- test/adafruit_strand_test/main.cpp
+++ test/adafruit_strand_test/main.cpp
@@ -7,31 +7,43 @@
 #define STRAND_2_PIXELS 120
 #define STRAND_3_PIXELS 96
 #define STRAND_4_PIXELS 72
+#define STRAND_4_PIXELS 48
+#define STRAND_4_PIXELS 24
 
 Adafruit_DotStar strip1(STRAND_1_PIXELS, STRAND_1_DATA_PIN, STRAND_1_CLOCK_PIN, DOTSTAR_BRG);
 Adafruit_DotStar strip2(STRAND_2_PIXELS, STRAND_2_DATA_PIN, STRAND_2_CLOCK_PIN, DOTSTAR_BRG);
 Adafruit_DotStar strip3(STRAND_3_PIXELS, STRAND_3_DATA_PIN, STRAND_3_CLOCK_PIN, DOTSTAR_BRG);
 Adafruit_DotStar strip4(STRAND_4_PIXELS, STRAND_4_DATA_PIN, STRAND_4_CLOCK_PIN, DOTSTAR_BRG);
+Adafruit_DotStar strip4(STRAND_5_PIXELS, STRAND_5_DATA_PIN, STRAND_5_CLOCK_PIN, DOTSTAR_BRG);
+Adafruit_DotStar strip4(STRAND_6_PIXELS, STRAND_6_DATA_PIN, STRAND_6_CLOCK_PIN, DOTSTAR_BRG);
 
 void setup() {
   strip1.begin();
   strip2.begin();
   strip3.begin();
   strip4.begin();
+  strip5.begin();
+  strip6.begin();
   strip1.show();
   strip2.show();
   strip3.show();
   strip4.show();
+  strip5.show();
+  strip6.show();
 }
 
 int head1 = 0, tail1 = -3;
 int head2 = 0, tail2 = -3;
 int head3 = 0, tail3 = -3;
 int head4 = 0, tail4 = -3;
+int head5 = 0, tail5 = -3;
+int head6 = 0, tail6 = -3;
 uint32_t color1 = 0xFF0000;
 uint32_t color2 = 0xFF0000;
 uint32_t color3 = 0xFF0000;
 uint32_t color4 = 0xFF0000;
+uint32_t color5 = 0xFF0000;
+uint32_t color6 = 0xFF0000;
 
 void loop() {
   strip1.setPixelColor(head1, color1);
@@ -46,6 +58,12 @@ void loop() {
   strip4.setPixelColor(head4, color4);
   strip4.setPixelColor(tail4, 0);
   strip4.show();
+  strip5.setPixelColor(head5, color5);
+  strip5.setPixelColor(tail5, 0);
+  strip5.show();
+  strip6.setPixelColor(head6, color6);
+  strip6.setPixelColor(tail6, 0);
+  strip6.show();
   delay(20);
 
   if (++head1 >= STRAND_1_PIXELS) {
@@ -78,4 +96,18 @@ void loop() {
   }
   if (++tail4 >= STRAND_4_PIXELS)
     tail4 = 0;
+  if (++head5 >= STRAND_5_PIXELS) {
+    head5 = 0;
+    if ((color5 >>= 8) == 0)
+      color5 = 0xFF0000;
+  }
+  if (++tail5 >= STRAND_5_PIXELS)
+    tail5 = 0;
+  if (++head6 >= STRAND_6_PIXELS) {
+    head6 = 0;
+    if ((color6 >>= 8) == 0)
+      color6 = 0xFF0000;
+  }
+  if (++tail6 >= STRAND_6_PIXELS)
+    tail6 = 0;
 }
--- test/fastled_strand_test/main.cpp
+++ test/fastled_strand_test/main.cpp
@@ -1,23 +1,30 @@
 #include <FastLED.h>
 
 #define ABIMC_V_1_3
+
 #include <abimc_pcb.h>
 
 #define STRAND_1_PIXELS 144
 #define STRAND_2_PIXELS 120
 #define STRAND_3_PIXELS 96
 #define STRAND_4_PIXELS 72
+#define STRAND_5_PIXELS 48
+#define STRAND_6_PIXELS 24
 
 CRGB strand1[STRAND_1_PIXELS];
 CRGB strand2[STRAND_2_PIXELS];
 CRGB strand3[STRAND_3_PIXELS];
 CRGB strand4[STRAND_4_PIXELS];
+CRGB strand5[STRAND_5_PIXELS];
+CRGB strand6[STRAND_6_PIXELS];
 
 void setup() {
   FastLED.addLeds<DOTSTAR, STRAND_1_DATA_PIN, STRAND_1_CLOCK_PIN, BRG>(strand1, STRAND_1_PIXELS);
   FastLED.addLeds<DOTSTAR, STRAND_2_DATA_PIN, STRAND_2_CLOCK_PIN, BRG>(strand2, STRAND_2_PIXELS);
   FastLED.addLeds<DOTSTAR, STRAND_3_DATA_PIN, STRAND_3_CLOCK_PIN, BRG>(strand3, STRAND_3_PIXELS);
   FastLED.addLeds<DOTSTAR, STRAND_4_DATA_PIN, STRAND_4_CLOCK_PIN, BRG>(strand4, STRAND_4_PIXELS);
+  FastLED.addLeds<DOTSTAR, STRAND_5_DATA_PIN, STRAND_5_CLOCK_PIN, BRG>(strand5, STRAND_5_PIXELS);
+  FastLED.addLeds<DOTSTAR, STRAND_6_DATA_PIN, STRAND_6_CLOCK_PIN, BRG>(strand6, STRAND_6_PIXELS);
   FastLED.clear();
 }
 
@@ -25,10 +32,14 @@ int head1 = 0, tail1 = -3;
 int head2 = 0, tail2 = -3;
 int head3 = 0, tail3 = -3;
 int head4 = 0, tail4 = -3;
+int head5 = 0, tail5 = -3;
+int head6 = 0, tail6 = -3;
 CHSV color1 = CHSV(0, 255, 127);
 CHSV color2 = CHSV(0, 255, 127);
 CHSV color3 = CHSV(0, 255, 127);
 CHSV color4 = CHSV(0, 255, 127);
+CHSV color5 = CHSV(0, 255, 127);
+CHSV color6 = CHSV(0, 255, 127);
 
 void loop() {
   strand1[head1] = color1;
@@ -39,6 +50,10 @@ void loop() {
   strand3[tail3] = CRGB::Black;
   strand4[head4] = color4;
   strand4[tail4] = CRGB::Black;
+  strand5[head5] = color5;
+  strand5[tail5] = CRGB::Black;
+  strand6[head6] = color6;
+  strand6[tail6] = CRGB::Black;
   FastLED.show();
   delay(20);
   if (++head1 >= STRAND_1_PIXELS) {
@@ -61,4 +76,19 @@ void loop() {
     color4.h = (color4.h + 224) % 256;
   }
   tail4 = (tail4 + 1) % STRAND_4_PIXELS;
+  if (++head4 >= STRAND_4_PIXELS) {
+    head4 %= STRAND_4_PIXELS;
+    color4.h = (color4.h + 224) % 256;
+  }
+  tail4 = (tail4 + 1) % STRAND_4_PIXELS;
+  if (++head5 >= STRAND_5_PIXELS) {
+    head5 %= STRAND_5_PIXELS;
+    color5.h = (color5.h + 224) % 256;
+  }
+  tail5 = (tail5 + 1) % STRAND_5_PIXELS;
+  if (++head6 >= STRAND_6_PIXELS) {
+    head6 %= STRAND_6_PIXELS;
+    color6.h = (color6.h + 224) % 256;
+  }
+  tail6 = (tail6 + 1) % STRAND_6_PIXELS;
 }
